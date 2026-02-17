ce245ecbcc30d6f45e6ea1dc47dc651f0f9935af
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-02-18 03:03:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (98.44%) - LeetSync

---
+++ 953-reverse-only-letters/reverse-only-letters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+        int i=0;
+        int size = s.length();
+        int j=size-1;
+
+        while( i <= j){
+            if( isalpha(s[i]) && isalpha(s[j])){
+               swap(s[i],s[j]);
+               i++;
+               j--;
+            }
+            else if(!isalpha(s[i])){
+                i++;
+            }
+            else if(!isalpha(s[j])){
+                j--;
+            }
+            
+        }
+        return s;
+        
+    }
+};
