4470c536e4da7479ccda782325444ebd034b172c
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-02-18 13:44:41 +0530

    Time: 9 ms (11.91%) | Memory: 23.9 MB (9.20%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     string reverseWords(string s) {
 
-        stringstream ss(s);
+        stringstream ss(s); // it reads until space
         string token = "";
         string result = "";
 
-        while ( ss >> token ){
+        while ( ss >> token ){ // providing input to token.
 
             result = token + " " + result;
 
         }
-        result=result.substr(0,result.length()-1);
+        result=result.substr(0,result.length()-1); // removing empty space of last position.
 
         return result;
         
