21616e2e0b92371b8fd7605debce7482faed442d
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-02-18 13:27:37 +0530

    Time: 9 ms (11.91%) | Memory: 24 MB (8.15%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        stringstream ss(s);
+        string token = "";
+        string result = "";
+
+        while ( ss >> token ){
+
+            result = token + " " + result;
+
+        }
+        result=result.substr(0,result.length()-1);
+
+        return result;
+        
+    }
+};
