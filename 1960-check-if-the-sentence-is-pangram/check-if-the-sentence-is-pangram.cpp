893ccad89d38e47b125af515e3fa7c0779325cd2
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-02-22 23:12:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (83.96%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        vector<int> arr(26, 0);
+
+      
+        for(int i = 0; i < sentence.length(); i++) {
+            int index = sentence[i] - 'a';
+            arr[index]++;
+        }
+
+        for(int i = 0; i < arr.size(); i++) {
+            if(arr[i] == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
