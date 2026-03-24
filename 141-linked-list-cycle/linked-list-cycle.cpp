35c8c783fd887ef158695e31bc12a0b75e797199
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-03-24 22:28:41 +0530

    Time: 9 ms (54.03%) | Memory: 11.9 MB (52.87%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+
+
+        // Optimal Approach: Floyd’s Cycle Detection (Tortoise & Hare).
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next->next; // Moving 2 steps.
+            slow = slow->next; // Moving 1 step.
+            if(slow == fast) {
+               return true;
+            }
+        }
+        
+
+        return false;
+        
+    }
+};
