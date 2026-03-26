6f3cebe1d4e6753069d74175a187639959971418
Shivansh Shukla <csai23156@glbitm.ac.in>
2026-03-26 22:28:05 +0530

    Time: 0 ms (100.00%) | Memory: 121.9 MB (42.34%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    ListNode* reverse(ListNode* head) { // finding reverse of Linked List.
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* forward = curr->next;
+
+        while(curr != NULL) {
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward; 
+        }
+
+        return prev;
+
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL) {
+            return true;
+        }
+        if(head->next == NULL) {
+            return true;
+        }
+        // Slow and fast approach. we find middle of LL.
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while(fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        // Now slow is at middle.
+        ListNode* NewHead = reverse(slow->next);
+
+        slow->next = NewHead;
+        // Now compare both LL left side and right side.
+        ListNode* temp1 = head;
+        ListNode* temp2 = NewHead;
+
+        while(temp2 != NULL) {
+            if(temp1->val != temp2->val) {
+                return false;
+            }
+            else { // Move forward. if data is equal. check new element.
+                temp1 = temp1->next;
+                temp2 = temp2->next;
+            }
+        }
+
+        return true;
+
+    }
+};
